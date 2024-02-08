13e8ec0d57a79a085e47011a14c0a54f0f52b3e0
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-02-08 18:51:08 +0700

    aaaa

--- api.php
+++ api.php
@@ -34,4 +34,61 @@ if ($_GET['act'] == 'profile') {
 
 }
 
+
+
+
+if($_GET['act'] == 'insert_owner_csv'){
+    $lokasi_file = $_FILES['filecsv']['tmp_name'];
+    if (!empty($lokasi_file)) {
+        $csvFile = fopen($_FILES['filecsv']['tmp_name'], 'r');
+        // skip baris pertama
+        fgetcsv($csvFile);
+            while (($getData = fgetcsv($csvFile, 1000, ",")) !== FALSE)
+                {
+                    $id = $getData[0];
+                    $kode_owner = $getData[1];            
+                    $nama = $getData[2];  
+                    $jenis_kelamin = $getData[3];           
+                    $alamat = $getData[4];
+                    $telpon = $getData[5];
+                    $email = $getData[6];
+                    $kelurahan = $getData[7];
+                    $kecamatan = $getData[8];
+                    $kota = $getData[9];
+                    $provinsi = $getData[10];
+                    $status = $getData[11];
+                    $id_pet = $getData[12];
+                    $no_wa = $getData[13];
+
+                    $check = mysqli_query($koneksi, "select id, kode_owner from tbl_owner where kode_owner = '" .$kode_owner. "'");
+
+                    if (mysqli_num_rows($check) > 0){
+                       $update =  mysqli_query($koneksi, "update tbl_owner set kode_owner='" .$kode_owner. "',nama='" . $nama . "', jenis_kelamin='" . $jenis_kelamin . "', alamat='" . $alamat . "',telpon='" . $telpon . "', email = '" . $email . "', kelurahan = '" . $kelurahan . "' , kecamatan = '" . $kecamatan . "' ,  kota = '" . $kota . "', provinsi = '" . $provinsi . "', status = '" . $status . "' , id_pet = '" . $id_pet . "', no_wa = '" . $no_wa . "' where kode_owner = '" . $kode_owner . "'");  
+                            if($update){
+                                $response = ["message" => "success updated"];
+                            }else{
+                                $response = ["message" => "update data failed"];
+                            }
+                    }else{
+                        $insert =  mysqli_query($koneksi, "insert into tbl_owner (kode_owner,nama, jenis_kelamin, alamat, telpon, email, kelurahan, kecamatan, kota, provinsi, status, id_pet, no_wa) VALUES ('" .$kode_owner. "', '" . $nama . "', '" .$jenis_kelamin. "', '" .$alamat. "', '" .$telpon. "', '" .$email. "', '" .$kelurahan. "', '" .$kecamatan. "', '" .$kota. "', '" .$provinsi. "', '" .$status. "' ,'" .$id_pet. "','" .$no_wa. "')");  
+                            if($insert){
+                                $response = ["message" => "success inserted"];
+                            }else{
+                                $response = ["message" => "insert data failed"];
+                            }
+                    }
+                }
+    } else {
+        $response = ["message" => "failed_uploaded"];   
+    }
+    echo json_encode($response);
+}
+
+// $amount = '12345.67';
+
+// $formatter = new NumberFormatter('en_GB',  NumberFormatter::CURRENCY);
+// echo 'UK: ', $formatter->formatCurrency($amount, 'EUR'), PHP_EOL;
+
+// $formatter = new NumberFormatter('de_DE',  NumberFormatter::CURRENCY);
+// echo 'DE: ', $formatter->formatCurrency($amount, 'EUR'), PHP_EOL;
 ?>
